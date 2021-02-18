40912c23d1f4fc25af22bc9546df8854256379a6
Aldin Mahmutbegovic <aldinmah@gmail.com>
2021-02-18 23:24:35 +0100

    prompt test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './main.css';
 import UltimaTVPlayer from './components/UltimaTVPlayer';
+import PWAInstallerPrompt from 'react-pwa-installer-prompt';
 
 function App() {
   return (
     <div className="UltimaTVApp">
+      <PWAInstallerPrompt
+        render={({ onClick }) => (
+          <button type="button" onClick={onClick}>
+            Install UltimaTV
+            </button>
+        )}
+        callback={(data) => console.log(data)}
+      />
       <img className="logo" src="/assets/images/logo.png" alt="logo" />
       <span className="title">PWA Shaka player example</span>
       <UltimaTVPlayer />
