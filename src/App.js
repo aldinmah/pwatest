cf314e6ed74fe94d3059881903008e56bc5304fa
Aldin Mahmutbegovic <aldinmah@gmail.com>
2021-02-18 23:36:00 +0100

    pwa prompt test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
 import './main.css';
 import UltimaTVPlayer from './components/UltimaTVPlayer';
-import PWAInstallerPrompt from 'react-pwa-installer-prompt';
+import usePWA from 'react-pwa-install-prompt'
 
 function App() {
+  const { isStandalone, isInstallPromptSupported, promptInstall } = usePWA()
+  const onClickInstall = async () => {
+    const didInstall = await promptInstall()
+    if (didInstall) {
+      // User accepted PWA install
+    }
+  }
+
+  const renderInstallButton = () => {
+    if (isInstallPromptSupported && isStandalone)
+
+      return (
+        <button onClick={onClickInstall}>Install UltimaTV</button>
+      )
+    return null
+  }
   return (
     <div className="UltimaTVApp">
-      <PWAInstallerPrompt
-        render={({ onClick }) => (
-          <button type="button" onClick={onClick}>
-            Install UltimaTV
-            </button>
-        )}
-        callback={(data) => console.log(data)}
-      />
-      <img className="logo" src="/assets/images/logo.png" alt="logo" />
+      <a href="/"><img className="logo" src="/assets/images/logo.png" alt="logo" /></a>
+      <div className="pwaInfo">
+      <br></br>
+      <span>PWA Info</span>
+      <br></br>
+      <br></br>
+      <span>Is Install Prompt Supported = <strong>{isInstallPromptSupported ? 'true' : 'false'}</strong></span>
+      <br></br>
+      <span>Is Standalone = {isStandalone ? 'true' : 'false'}</span>
+      {renderInstallButton()}
+      </div>
       <span className="title">PWA Shaka player example</span>
       <UltimaTVPlayer />
     </div>
